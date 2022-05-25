0b653c03599b3b8dfab1f275dd7280861115196b
Vince Nery II <58459176+Calixed@users.noreply.github.com>
2022-05-25 19:28:43 +0800

    Located the urls of customer_portal to the project url - Created the user with modified fields - Link app urls to the project urls

--- customer_portal/models.py
+++ customer_portal/models.py
@@ -1,2 +1,26 @@
 from django.db import models
+from django import forms
+# packages in helping creating user form and login form
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
+# Model of our User and How they will register
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(label = "First name")
+    last_name = forms.CharField(label = "Last Name")
+    email = forms.EmailField(label = "Email")
+
+    # properties of this class
+    class Meta:
+        model = User
+        fields = ("first_name","last_name" ,"email" )
+
+    # overriding the saving method, setting hte username as email
+    def save(self, commit= True):
+        user = super(RegisterForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        user.email = self.cleaned_data["email"]
+        user.username = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
--- customer_portal/views.py
+++ customer_portal/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django import forms
+from django.urls import reverse_lazy 
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-# Create your views here.
-class LoginCusome(LoginView)
+
+
+
+
+
+
