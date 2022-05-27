7e7aadfd2a3c79c75fb8d1efb540107c1cfa1f61
Vince Nery II <58459176+Calixed@users.noreply.github.com>
2022-05-27 23:42:36 +0800

    Removed the detailed overview in the admin dashboard

--- customer_portal/admin.py
+++ customer_portal/admin.py
@@ -1,18 +1,9 @@
 from django.contrib import admin
 from .models import Product, ProductCategory, Orders
 
-
-
-class OrdersAdmin(admin.ModelAdmin):
-    list_display = ('id','user', 'product_rented', 'days' ,'rent')
-
-class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id','product_name', 'product_price' ,'product_category')
-
-
 # Register your models here.
-admin.site.register(Product,ProductAdmin)
+admin.site.register(Product)
 admin.site.register(ProductCategory)
-admin.site.register(Orders, OrdersAdmin)
+admin.site.register(Orders)
 
 
